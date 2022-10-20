7fb6abc92cc9ecb7bbc4a7e76ca47ad737e2c6dc
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:42:50 -0300

    Update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,3 +1,4 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 preco = float(input('Preço do produto: R$'))
 novoPreco = preco - (preco * 5/100)
 print('Novo preço com 5% de desconto: R${:.2f}'.format(novoPreco))
