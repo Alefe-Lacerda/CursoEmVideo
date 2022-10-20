0bce107bdb7838b7ddfd14cdf57cb44126087f3f
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:53:26 -0300

    Update ex020.py

--- ex020.py
+++ ex020.py
@@ -1,3 +1,6 @@
+# O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. 
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
 from random import shuffle
 aluno1 = input('Aluno 1: ')
 aluno2 = input('Aluno 2: ')
