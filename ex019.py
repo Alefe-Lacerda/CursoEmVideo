18ff4ea2310274bfb1082237cd29440a5cd1bb92
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:51:11 -0300

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -1,3 +1,5 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+# Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 from random import choice
 aluno1 = input('Aluno 1: ')
 aluno2 = input('Aluno 2: ')
