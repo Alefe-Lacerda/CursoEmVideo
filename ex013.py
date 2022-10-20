2697826749d2225ee535a9b28fba4948ea1a7ec9
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:43:34 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,3 +1,4 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 funcionario = input('Nome do Funcionário: ')
 salario = float(input('Salário de {}: R$'.format(funcionario)))
 novoSalario = salario + (salario* 15/100)
