e35cf284aadb7fb1dd9fb93292d4229fc033bbfe
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:48:03 -0300

    Update ex017.py

--- ex017.py
+++ ex017.py
@@ -1,3 +1,4 @@
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
 from math import hypot
 c_o = float(input('Cateto oposto: '))
 c_a = float(input('Cateto adjacente: '))
