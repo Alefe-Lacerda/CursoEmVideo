86c259b3be9214a2bb72597837fa61a8d7b382fb
Alefe-Lacerda <95290349+Alefe-Lacerda@users.noreply.github.com>
2022-10-20 12:34:59 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,3 +1,4 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 n1 = float(input('Digite a nota 1: '))
 n2 = float(input('Digite a nota 2: '))
 mf = (n1 + n2)/2
